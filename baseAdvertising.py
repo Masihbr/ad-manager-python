a0e508c62e04bc6ff64fbf944fe70689bcbef225
Masihbr <masihbr@gmail.com>
2021-01-09 13:09:19 +0330

    added BaseAdvertising methods

--- baseAdvertising.py
+++ baseAdvertising.py
@@ -4,3 +4,19 @@ class BaseAdvertising:
         _id = 0
         _clicks = 0
         _views = 0
+
+    def getClicks(self):
+        return self._clicks
+
+    def getViews(self):
+        return self._views
+
+    def incClicks(self):
+        self._clicks += 1
+
+    def incViews(self):
+        self._views += 1
+
+    def describeMe(self):
+        return "parent class for Ad and Advertiser class"
+    
