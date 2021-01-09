93067aea0df9075d451f8a2822e54716284e23f8
Masihbr <masihbr@gmail.com>
2021-01-09 12:32:23 +0330

    added Ad constructor

--- ad.py
+++ ad.py
@@ -1,2 +1,12 @@
-class Ad:
-    pass
+from baseAdvertising import BaseAdvertising
+
+
+class Ad(BaseAdvertising):
+
+    def __init__(self, id, title, imgUrl, link, advertiser):
+        super().__init__()
+        self._id = id;
+        self.__title = title;
+        self.__imgUrl = imgUrl;
+        self.__link = link;
+        self.__advertiser = advertiser;
