0719fadc6ac018eb4a5d1dcb4b47ab9b05199e18
Masihbr <masihbr@gmail.com>
2021-01-09 13:15:15 +0330

    added new format for names

--- baseAdvertising.py
+++ baseAdvertising.py
@@ -5,17 +5,17 @@ class BaseAdvertising:
         _clicks = 0
         _views = 0
 
-    def getClicks(self):
+    def get_clicks(self):
         return self._clicks
 
-    def getViews(self):
+    def get_views(self):
         return self._views
 
-    def incClicks(self):
+    def inc_clicks(self):
         self._clicks += 1
 
-    def incViews(self):
+    def inc_views(self):
         self._views += 1
 
-    def describeMe(self):
+    def describe_me(self):
         return "parent class for Ad and Advertiser class"
