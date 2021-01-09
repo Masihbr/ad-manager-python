d4ad2b95c0a04fb34fab4aaddf16573de239d799
Masihbr <masihbr@gmail.com>
2021-01-09 12:46:50 +0330

    added Advertiser consturctor

--- advertiser.py
+++ advertiser.py
@@ -1,2 +1,11 @@
-class Advertiser:
-    pass
+from baseAdvertising import BaseAdvertising
+
+
+class Advertiser(BaseAdvertising):
+    __allAdvertisers = []
+
+    def __init__(self, id, name):
+        super().__init__()
+        self._id = id
+        self.__name = name
+        Advertiser.__allAdvertisers.append(self)
